da55e106d5d4b05e55495955d69540a920aa4724
Shubham <sp4363619@gmail.com>
2024-11-06 06:18:15 -0800

    Second commit

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -7,17 +7,19 @@ import pressure from "../../assets/Image/pressure.svg";
 import feels from "../../assets/Image/feels.svg";
 import visible from "../../assets/Image/visible.png";
 import heat from "../../assets/Image/he.png";
-import Sunny from "../../assets/Image/sunny.png"
-import Overcast from "../../assets/Image/overcast.png"
-import Mist from "../../assets/Image/mist.png"
-import Partly_Cloud from "../../assets/Image/partly.png"
-import Cloudy from "../../assets/Image/cloudy.png"
+import Sunny from "../../assets/Image/sunny.png";
+import Overcast from "../../assets/Image/overcast.png";
+import Mist from "../../assets/Image/mist.png";
+import Partly_Cloud from "../../assets/Image/partly.png";
+import Cloudy from "../../assets/Image/cloudy.png";
 
 function Card() {
   const { name } = useParams();
   //   console.log(name);
 
   const [Cvalue, setCvalue] = useState({});
+  const [Fvalue, setFvalue] = useState([]);
+
   useEffect(() => {
     const valueCountry = async () => {
       try {
@@ -26,6 +28,13 @@ function Card() {
         );
 
         setCvalue(val.data);
+
+        const fval = await axios.get(
+          `https://api.weatherapi.com/v1/forecast.json?key=fd357a9216c745dba4361315240511&q=${name}&days=5`
+        );
+
+        setFvalue(fval.data.forecast.forecastday);
+        console.log(fval.data.forecast.forecastday);
       } catch (err) {
         console.log(err.message);
       }
@@ -45,47 +54,59 @@ function Card() {
     if (condition.includes("cloudy")) return Cloudy;
     if (condition.includes("mist")) return Mist;
     if (condition.includes("partly cloudy")) return Partly_Cloud;
-    return Sunny; 
+    return Sunny;
   };
 
   const backgroundImage = {
     backgroundImage: `url(${getBackgroundImage()})`,
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-    minHeight: '100vh',
-    color: 'white',  
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    minHeight: "100vh",
+    color: "white",
   };
 
   const localDateTime = new Date(Cvalue.location.localtime);
-  const formattedLocalTime = localDateTime.toLocaleString('en-US', {
-    weekday: 'long', 
-    year: 'numeric',
-    month: 'long',   
-    day: 'numeric',
-    hour: '2-digit', 
-    minute: '2-digit',
-    hour12: true,   
+  const formattedLocalTime = localDateTime.toLocaleString("en-US", {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: true,
   });
-
 
   return (
     <>
-      <div className="min-h-screen flex flex-col items-center justify-center" style={backgroundImage} >
+      <div
+        className="min-h-screen flex flex-col items-center justify-center"
+        style={backgroundImage}
+      >
         <h1 className="text-4xl font-semibold py-5 text-center">
           Weather Details
         </h1>
 
-        <div className="flex flex-col bg-white rounded p-8 w-full max-w-xs shadow-lg">
-          <div className="font-bold text-xl text-black">{Cvalue.location.name}</div>
+        <div className="flex flex-col bg-white rounded p-8 w-full max-w-xs shadow-lg ring-8 ring-white ring-opacity-40">
+          <div className="font-bold text-xl text-black">
+            {Cvalue.location.name}
+          </div>
           <div className="text-sm text-gray-500">{formattedLocalTime}</div>
           <div className="mt-6 self-center inline-flex items-center justify-center w-36 h-36">
-            <img src={Cvalue.current.condition.icon} alt="weatherIcon" className="object-contain w-full h-full" />
+            <img
+              src={Cvalue.current.condition.icon}
+              alt="weatherIcon"
+              className="object-contain w-full h-full"
+            />
           </div>
           <div className="flex flex-row items-center justify-center mt-6">
-            <div className="font-medium text-6xl text-black">{Cvalue.current.temp_c}°</div>
+            <div className="font-medium text-6xl text-black">
+              {Cvalue.current.temp_c}°
+            </div>
             <div className="flex flex-col items-center ml-6">
               <div>
-                <h1 className="text-2xl text-gray-400 font-semibold text-nowrap">{Cvalue.current.condition.text}</h1>
+                <h1 className="text-2xl text-gray-400 font-semibold text-nowrap">
+                  {Cvalue.current.condition.text}
+                </h1>
               </div>
               <div className="mt-1">
                 <span className="text-sm">
@@ -125,7 +146,9 @@ function Card() {
               </div>
               <div>
                 <div className="font-medium text-lg text-black">Pressure</div>
-                <div className="text-sm text-gray-500">{Cvalue.current.pressure_mb} mb</div>
+                <div className="text-sm text-gray-500">
+                  {Cvalue.current.pressure_mb} mb
+                </div>
               </div>
             </div>
             <div className="flex items-center gap-2">
@@ -134,7 +157,9 @@ function Card() {
               </div>
               <div>
                 <div className="font-medium text-lg text-black">Visibility</div>
-                <div className="text-sm text-gray-500">{Cvalue.current.vis_km} km</div>
+                <div className="text-sm text-gray-500">
+                  {Cvalue.current.vis_km} km
+                </div>
               </div>
             </div>
             <div className="flex items-center gap-2">
@@ -143,10 +168,33 @@ function Card() {
               </div>
               <div>
                 <div className="font-medium text-lg text-black">Humidity</div>
-                <div className="text-sm text-gray-500">{Cvalue.current.humidity} %</div>
+                <div className="text-sm text-gray-500">
+                  {Cvalue.current.humidity} %
+                </div>
               </div>
             </div>
           </div>
+        </div>
+
+        <div className="flex flex-col space-y-6 w-full max-w-screen-sm bg-white p-10 mt-10 rounded-xl ring-8 ring-white ring-opacity-40 mb-14">
+          {Fvalue.map((fele) => (
+            <div
+              className="flex justify-between items-center"
+              key={Fvalue.date_epoch}
+            >
+              <span className="font-semibold text-lg w-1/4 text-black">
+              {new Date(fele.date).toLocaleDateString("en-US", { weekday: "short", month: "short", day: "numeric" })}
+              </span>
+              <div className="flex items-center justify-end w-1/4 pr-10 gap-3">
+                <span className="font-semibold text-black">{fele.day.avghumidity} %</span>
+                <img src={humidity} alt="weather-icon" className="w-7 h-7" />
+              </div>
+              <img src={fele.day.condition.icon} alt="weather-icon" className="w-9 h-9" />
+              <span className="font-semibold text-lg w-1/4 text-right text-black">
+              {fele.day.avgtemp_c} °C / {fele.day.avgtemp_f} °F
+              </span>
+            </div>
+          ))}
         </div>
       </div>
     </>
